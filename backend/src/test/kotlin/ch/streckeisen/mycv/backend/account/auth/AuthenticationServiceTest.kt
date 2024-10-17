88d597ad12909bf29835e9063115baa997574963
Lukas Streckeisen <lukas@streckeisen.dev>
2024-10-17 18:51:47 +0200

    fix build

--- backend/src/test/kotlin/ch/streckeisen/mycv/backend/account/auth/AuthenticationServiceTest.kt
+++ backend/src/test/kotlin/ch/streckeisen/mycv/backend/account/auth/AuthenticationServiceTest.kt
@@ -31,12 +31,10 @@ private val VALID_SIGNUP_REQUEST = SignupRequestDto(
     "3287",
     "City",
     "CH",
-    true,
     "a*c3efgH"
 )
 
 private val INVALID_SIGNUP_REQUEST = SignupRequestDto(
-    null,
     null,
     null,
     null,
