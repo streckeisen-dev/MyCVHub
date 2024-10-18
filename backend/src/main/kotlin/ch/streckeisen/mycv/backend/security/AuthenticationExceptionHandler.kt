799ae13832e579e6753d7d1200915101245944a9
Lukas Streckeisen <lukas@streckeisen.dev>
2024-10-18 18:39:37 +0200

    make sure auth errors return proper response code

---
+++ backend/src/main/kotlin/ch/streckeisen/mycv/backend/security/AuthenticationExceptionHandler.kt
@@ -0,0 +1,16 @@
+package ch.streckeisen.mycv.backend.security
+
+import jakarta.servlet.http.HttpServletRequest
+import jakarta.servlet.http.HttpServletResponse
+import org.springframework.security.core.AuthenticationException
+import org.springframework.security.web.AuthenticationEntryPoint
+
+class AuthenticationExceptionHandler : AuthenticationEntryPoint {
+    override fun commence(
+        request: HttpServletRequest,
+        response: HttpServletResponse,
+        authException: AuthenticationException
+    ) {
+        response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Unauthorized")
+    }
+}
---
+++ backend/src/main/kotlin/ch/streckeisen/mycv/backend/security/MyCVAccessDeniedHandler.kt
@@ -0,0 +1,16 @@
+package ch.streckeisen.mycv.backend.security
+
+import jakarta.servlet.http.HttpServletRequest
+import jakarta.servlet.http.HttpServletResponse
+import org.springframework.security.access.AccessDeniedException
+import org.springframework.security.web.access.AccessDeniedHandler
+
+class MyCVAccessDeniedHandler : AccessDeniedHandler {
+    override fun handle(
+        request: HttpServletRequest,
+        response: HttpServletResponse,
+        accessDeniedException: AccessDeniedException
+    ) {
+        response.sendError(HttpServletResponse.SC_FORBIDDEN, "Forbidden")
+    }
+}
