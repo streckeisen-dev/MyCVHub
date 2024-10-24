0c3315c2bdc9f932c422b43d8384d322473e14a3
Lukas Streckeisen <lukas@streckeisen.dev>
2024-10-24 16:59:25 +0200

    load locale messages without top-level await

--- frontend/src/plugins/i18n.ts
+++ frontend/src/plugins/i18n.ts
@@ -1,25 +1,26 @@
 import { createI18n } from 'vue-i18n'
 import LanguageService from '@/services/LanguageService'
 
+type NestedMessage = {
+  [key: string]: string | NestedMessage
+}
+
+type Messages = {
+  [locale: string]: NestedMessage
+}
+
+const messages: Messages = {}
+const langModules = import.meta.glob('../locales/*.json', { eager: true })
+Object.keys(langModules).forEach((filePath) => {
+  const lang = filePath.replace('../locales/', '').replace('.json', '')
+  messages[lang] = langModules[filePath] as NestedMessage
+})
+
 const i18n = createI18n({
   locale: LanguageService.getLanguage(),
   fallbackLocale: 'en',
   legacy: false,
-  messages: {}
+  messages
 })
-
-const availableLocales = Object.keys(import.meta.glob('../locales/*.json')).map((file) => {
-  const match = file.match(/\.\.\/locales\/(.*)\.json$/)
-  if (match) {
-    return match[1]
-  }
-  return null
-}).filter(l => l != null)
-console.log('availableLangs', availableLocales)
-
-for (let availableLocale of availableLocales) {
-  const messages = await import(`../locales/${availableLocale}.json`)
-  i18n.global.setLocaleMessage(availableLocale, messages)
-}
 
 export default i18n
