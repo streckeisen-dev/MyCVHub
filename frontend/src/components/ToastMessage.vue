5111c96844840567e9a7a947e2fb8b2002b9f270
Lukas Streckeisen <lukas@streckeisen.dev>
2024-10-28 13:53:01 +0100

    add toast service

---
+++ frontend/src/components/ToastMessage.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <template v-if="details">
+      <h4>{{ message }}</h4>
+      <p>{{ details }}</p>
+    </template>
+    <p v-else>{{ message }}</p>
+  </div>
+</template>
+
+<script setup lang="ts">
+defineProps<{
+  message: string
+  details?: string
+}>()
+</script>
+
+<style scoped lang="scss"></style>
