74b17bb165f67e627f2e144b95540566bddb9c14
Lukas Streckeisen <lukas@streckeisen.dev>
2024-11-17 13:45:39 +0100

    add mail properties, rename security properties

--- backend/src/main/kotlin/ch/streckeisen/mycv/backend/security/JwtService.kt
+++ backend/src/main/kotlin/ch/streckeisen/mycv/backend/security/JwtService.kt
@@ -12,11 +12,11 @@ import javax.crypto.SecretKey
 
 @Service
 class JwtService(
-    @Value("\${security.jwt.secret}")
+    @Value("\${my-cv.security.jwt.secret}")
     private val jwtSecret: String,
-    @Value("\${security.jwt.access.expiration-time}")
+    @Value("\${my-cv.security.jwt.access.expiration-time}")
     private val jwtAccessExpirationTime: Long,
-    @Value("\${security.jwt.refresh.expiration-time}")
+    @Value("\${my-cv.security.jwt.refresh.expiration-time}")
     private val jwtRefreshExpirationTime: Long
 ) {
     fun extractUsername(token: String): String? {
