ec7485c64c11785d4dcf2e2f2bf9208b396a68ce
Lukas Streckeisen <lukas@streckeisen.dev>
2025-01-26 18:02:31 +0100

    generate cypress test results and capture them in CI pipeline

--- frontend/cypress.config.ts
+++ frontend/cypress.config.ts
@@ -6,6 +6,13 @@ export default defineConfig({
       framework: 'vue',
       bundler: 'vite'
     },
+    reporter: 'mochawesome',
+    reporterOptions: {
+      reportDir: 'cypress/results',
+      overwrite: true,
+      html: false,
+      json: true
+    },
     specPattern: 'cypress/component/**/*.cy.ts',
     supportFile: 'cypress/support/component.ts'
   }
