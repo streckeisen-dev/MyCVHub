958c8989579fdb622839a5d90c8fa52a814e3a1a
Lukas Streckeisen <lukas@streckeisen.dev>
2025-01-28 11:18:37 +0100

    refactoring

--- frontend/tests/services/RouteSecurityService.test.ts
+++ frontend/tests/services/RouteSecurityService.test.ts
@@ -1,7 +1,41 @@
-import { afterEach, expect, describe, it, vi } from 'vitest'
+import { afterEach, describe, expect, it, MockInstance, vi } from 'vitest'
 import AccountApi from '../../src/api/AccountApi'
-import LoginStateService, { ACCOUNT_STATUS_KEY } from '../../src/services/LoginStateService'
+import LoginStateService from '../../src/services/LoginStateService'
 import RouteSecurityService from '../../src/services/RouteSecurityService'
+import { NavigationGuardNext } from 'vue-router'
+
+interface RouteSecurityTestResources {
+  loadAccountStatusSpy: MockInstance<() => Promise<void>>
+  next: NavigationGuardNext
+}
+
+function prepareTestResources(
+  shouldAccountStatusLoadSucceed: boolean,
+  stateAccountStatus: string
+): RouteSecurityTestResources {
+  const loadAccountStatusSpy = mockLoadAccountStatus(shouldAccountStatusLoadSucceed)
+  mockStateAccountStatus(stateAccountStatus)
+  const nextMock = vi.fn()
+  return {
+    loadAccountStatusSpy: loadAccountStatusSpy,
+    next: nextMock
+  }
+}
+
+function mockStateAccountStatus(status: string) {
+  vi.spyOn(LoginStateService, 'getAccountStatus').mockImplementation(() => status)
+}
+
+function mockLoadAccountStatus(shouldSucceed: boolean): MockInstance<() => Promise<void>> {
+  const accountApiSpy = vi.spyOn(AccountApi, 'loadAccountStatus')
+  accountApiSpy.mockImplementation(() => {
+    if (shouldSucceed) {
+      return Promise.resolve()
+    }
+    return Promise.reject()
+  })
+  return accountApiSpy
+}
 
 describe('enforceRouteAccessPermissions', () => {
   afterEach(() => {
@@ -10,115 +44,77 @@ describe('enforceRouteAccessPermissions', () => {
   })
 
   it('should load account status if account status is not loaded', async () => {
-    const accountApiSpy = vi.spyOn(AccountApi, 'loadAccountStatus')
-    vi.spyOn(LoginStateService, 'getAccountStatus').mockImplementation(() => 'VERIFIED')
-    accountApiSpy.mockImplementation(() => {
-      return Promise.resolve()
-    })
-    const nextMock = vi.fn()
+    const testResources = prepareTestResources(true, 'VERIFIED')
 
-    await RouteSecurityService.enforceRouteAccessPermissions(undefined, undefined, nextMock)
+    await RouteSecurityService.enforceRouteAccessPermissions(undefined, undefined, testResources.next)
 
-    expect(accountApiSpy).toHaveBeenCalledTimes(1)
-    expect(nextMock).toHaveBeenCalledTimes(1)
-    expect(nextMock).toHaveBeenCalledWith()
+    expect(testResources.loadAccountStatusSpy).toHaveBeenCalledTimes(1)
+    expect(testResources.next).toHaveBeenCalledTimes(1)
+    expect(testResources.next).toHaveBeenCalledWith()
   })
 
   it('should not load account status if account status is verified', async () => {
-    const accountApiSpy = vi.spyOn(AccountApi, 'loadAccountStatus')
-    localStorage.setItem(ACCOUNT_STATUS_KEY, 'VERIFIED')
-    accountApiSpy.mockImplementation(() => {
-      return Promise.resolve()
-    })
-    const nextMock = vi.fn()
+    const testResources = prepareTestResources(true, 'VERIFIED')
 
-    await RouteSecurityService.enforceRouteAccessPermissions(undefined, 'VERIFIED', nextMock)
+    await RouteSecurityService.enforceRouteAccessPermissions(undefined, 'VERIFIED', testResources.next)
 
-    expect(accountApiSpy).toHaveBeenCalledTimes(0)
-    expect(nextMock).toHaveBeenCalledTimes(1)
-    expect(nextMock).toHaveBeenCalledWith()
+    expect(testResources.loadAccountStatusSpy).toHaveBeenCalledTimes(0)
+    expect(testResources.next).toHaveBeenCalledTimes(1)
+    expect(testResources.next).toHaveBeenCalledWith()
   })
 
   it('should redirect to account-verification-pending page if account status load fails', async () => {
-    const accountApiSpy = vi.spyOn(AccountApi, 'loadAccountStatus')
-    accountApiSpy.mockImplementation(() => {
-      return Promise.reject()
-    })
-    const nextMock = vi.fn()
+    const testResources = prepareTestResources(false, undefined)
 
-    await RouteSecurityService.enforceRouteAccessPermissions(undefined, undefined, nextMock)
+    await RouteSecurityService.enforceRouteAccessPermissions(undefined, undefined, testResources.next)
 
-    expect(accountApiSpy).toHaveBeenCalledTimes(1)
-    expect(nextMock).toHaveBeenCalledTimes(1)
-    expect(nextMock).toHaveBeenCalledWith({ name: 'account-verification-pending' })
+    expect(testResources.loadAccountStatusSpy).toHaveBeenCalledTimes(1)
+    expect(testResources.next).toHaveBeenCalledTimes(1)
+    expect(testResources.next).toHaveBeenCalledWith({ name: 'account-verification-pending' })
   })
 
   it('should redirect to account-verification-pending page if account status load returns invalid result', async () => {
-    const accountApiSpy = vi.spyOn(AccountApi, 'loadAccountStatus')
-    vi.spyOn(LoginStateService, 'getAccountStatus').mockImplementation(() => undefined)
-    accountApiSpy.mockImplementation(() => {
-      return Promise.resolve()
-    })
-    const nextMock = vi.fn()
+    const testResources = prepareTestResources(true, undefined)
 
-    await RouteSecurityService.enforceRouteAccessPermissions(undefined, undefined, nextMock)
+    await RouteSecurityService.enforceRouteAccessPermissions(undefined, undefined, testResources.next)
 
-    expect(accountApiSpy).toHaveBeenCalledTimes(1)
-    expect(nextMock).toHaveBeenCalledTimes(1)
-    expect(nextMock).toHaveBeenCalledWith({ name: 'account-verification-pending' })
+    expect(testResources.loadAccountStatusSpy).toHaveBeenCalledTimes(1)
+    expect(testResources.next).toHaveBeenCalledTimes(1)
+    expect(testResources.next).toHaveBeenCalledWith({ name: 'account-verification-pending' })
   })
 
   it('should redirect to account-verification-pending page if route requires verified account and account is not verified', async () => {
-    const accountApiSpy = vi.spyOn(AccountApi, 'loadAccountStatus')
-    vi.spyOn(LoginStateService, 'getAccountStatus').mockImplementation(() => 'UNVERIFIED')
-    accountApiSpy.mockImplementation(() => {
-      return Promise.resolve()
-    })
-    const nextMock = vi.fn()
+    const testResources = prepareTestResources(true, 'UNVERIFIED')
 
-    await RouteSecurityService.enforceRouteAccessPermissions('VERIFIED', 'UNVERIFIED', nextMock)
+    await RouteSecurityService.enforceRouteAccessPermissions('VERIFIED', 'UNVERIFIED', testResources.next)
 
-    expect(nextMock).toHaveBeenCalledTimes(1)
-    expect(nextMock).toHaveBeenCalledWith({ name: 'account-verification-pending' })
+    expect(testResources.next).toHaveBeenCalledTimes(1)
+    expect(testResources.next).toHaveBeenCalledWith({ name: 'account-verification-pending' })
   })
 
   it('should let user access route if route requires verified account and account is verified', async () => {
-    const accountApiSpy = vi.spyOn(AccountApi, 'loadAccountStatus')
-    vi.spyOn(LoginStateService, 'getAccountStatus').mockImplementation(() => 'VERIFIED')
-    accountApiSpy.mockImplementation(() => {
-      return Promise.resolve()
-    })
-    const nextMock = vi.fn()
+    const testResources = prepareTestResources(true, 'VERIFIED')
 
-    await RouteSecurityService.enforceRouteAccessPermissions('VERIFIED', 'VERIFIED', nextMock)
-    expect(nextMock).toHaveBeenCalledTimes(1)
-    expect(nextMock).toHaveBeenCalledWith()
+    await RouteSecurityService.enforceRouteAccessPermissions('VERIFIED', 'VERIFIED', testResources.next)
+    expect(testResources.next).toHaveBeenCalledTimes(1)
+    expect(testResources.next).toHaveBeenCalledWith()
   })
 
   it('should redirect to oauth-signup page if route requires unverified account and account is incomplete', async () => {
-    const accountApiSpy = vi.spyOn(AccountApi, 'loadAccountStatus')
-    vi.spyOn(LoginStateService, 'getAccountStatus').mockImplementation(() => 'INCOMPLETE')
-    accountApiSpy.mockImplementation(() => {
-      return Promise.resolve()
-    })
-    const nextMock = vi.fn()
+    const testResources = prepareTestResources(true, 'INCOMPLETE')
 
-    await RouteSecurityService.enforceRouteAccessPermissions('UNVERIFIED', 'INCOMPLETE', nextMock)
+    await RouteSecurityService.enforceRouteAccessPermissions('UNVERIFIED', 'INCOMPLETE', testResources.next)
 
-    expect(nextMock).toHaveBeenCalledTimes(1)
-    expect(nextMock).toHaveBeenCalledWith({ name: 'oauth-signup' })
+    expect(testResources.next).toHaveBeenCalledTimes(1)
+    expect(testResources.next).toHaveBeenCalledWith({ name: 'oauth-signup' })
   })
 
   it('should let user access route if route requires unverified account and account is unverified', async () => {
-    vi.spyOn(AccountApi, 'loadAccountStatus').mockImplementation(() => {
-      return Promise.resolve()
-    })
-    vi.spyOn(LoginStateService, 'getAccountStatus').mockImplementation(() => 'UNVERIFIED')
-    const nextMock = vi.fn()
+    const testResources = prepareTestResources(true, 'UNVERIFIED')
 
-    await RouteSecurityService.enforceRouteAccessPermissions('UNVERIFIED', 'UNVERIFIED', nextMock)
+    await RouteSecurityService.enforceRouteAccessPermissions('UNVERIFIED', 'UNVERIFIED', testResources.next)
 
-    expect(nextMock).toHaveBeenCalledTimes(1)
-    expect(nextMock).toHaveBeenCalledWith()
+    expect(testResources.next).toHaveBeenCalledTimes(1)
+    expect(testResources.next).toHaveBeenCalledWith()
   })
 })
